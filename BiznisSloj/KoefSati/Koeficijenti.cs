c08b9a9def4f7b524cbc26929800d607af722489
Darko Eller <darko.eler@petrokemija.hr>
2017-07-31 10:35:19 +0200

    Koeficijenti public,private

--- BiznisSloj/KoefSati/Koeficijenti.cs
+++ BiznisSloj/KoefSati/Koeficijenti.cs
@@ -6,10 +6,10 @@ namespace BiznisSloj.KoefSati
 {
     public class Koeficijenti
     {
-        private byte Id { get; set; }
-        private string Sifra { get; set; }
+        public byte Id { get; set; }
+        public string Sifra { get; set; }
         public string Naziv { get; set; }
-        private decimal Koeficijent { get; set; }
+        public decimal Koeficijent { get; set; }
 
         public static decimal VratiIznos(string naziv)
         {
