2baa35ee6c44ebcc15376a2ba81d2cf6fb7078d8
NJUFRA-PC\Darko <darko.eler@petrokemija.hr>
2018-05-23 21:52:41 +0200

    PopravljenTečajniZahtjev23.05

--- BiznisSloj/BankarskiTecaj/TecajHNBa.cs
+++ BiznisSloj/BankarskiTecaj/TecajHNBa.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Windows;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -11,47 +12,55 @@ namespace BiznisSloj.BankarskiTecaj
 {
     public struct TecajHnBa : ITecaj
     {
-        public decimal VratiEuro()
+        public  decimal VratiEuro()
         {
-            //try
-            //{   
-            //    var client = new WebClient();
-            //    var web = FormirajWebString();
-            //    var content = client.DownloadString(web);
-            //    content = content.Replace(".", ",");
-            //    if (string.IsNullOrEmpty(content))
-            //    {
-            //       decimal.TryParse(content, out var tecaj);
-            //       return tecaj; 
-            //    }
-                    try
-                    {
-                        NadjiSaWebaHNBa();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                        throw;
-                    }
-            //}
-            //catch (Exception e)
-            //{
-            //    MessageBox.Show(
-            //        "Došlo je do pogreške prilikom prezimanja podatka, \n provjerite da li imate pristup internetu"
-            //        , "Upozorenje", MessageBoxButton.OK, MessageBoxImage.Warning);
-            //    return 0.0m;
-            //}
-
-            return 0;
+            try
+            {
+                var hnbTecaj = NadjiSaWebaHnBa();
+                if (hnbTecaj != 0.0m)
+                {
+                    return hnbTecaj;
+                }
+                var tecaj = VratiTecajSaNabaveNeta();
+                return tecaj;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(
+                    "Došlo je do pogreške prilikom prezimanja podatka, \n provjerite da li imate pristup internetu"
+                    , "Upozorenje", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return 0.0m;
+            }
         }
 
-        private void NadjiSaWebaHNBa()
+        private static decimal VratiTecajSaNabaveNeta()
         {
+            var client = new WebClient();
+            var web = FormirajWebString();
+            var content = client.DownloadString(web);
+            content = content.Replace(".", ",");
+            if (!string.IsNullOrEmpty(content)) return 0;
+            decimal.TryParse(content, out var tecaj);
+            return tecaj;
+
+        }
+
+        private static decimal NadjiSaWebaHnBa()
+        {
+            var tecaj = 0.0m;
             var jsonObject = new WebClient().DownloadString(@"http://api.hnb.hr/tecajn/v1?valuta=EUR");
             var rss =JArray.Parse(jsonObject);
-            //var token = rss.SelectToken()
-            //var nasao = rss.SelectToken("Srednji za devize").HasValues;
-
+            foreach (var parsedObject in rss.Children<JObject>())
+            {
+                foreach (var parsedProperty in parsedObject.Properties())
+                {
+                    var propertyName = parsedProperty.Name;
+                    if (!propertyName.Equals("Srednji za devize")) continue;
+                    var propertyValue = (string)parsedProperty.Value;
+                    decimal.TryParse(propertyValue, out tecaj);
+                }
+            }
+            return tecaj;
         }
 
         private static string FormirajWebString()
