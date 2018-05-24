052a3af9265765ce5c06dbfcee776809ed446a8d
Darko Eller <darko.eler@petrokemija.hr>
2018-05-24 12:49:50 +0200

    PopravakUJusing (reverted from commit 68c4b110bd30e0835ff7ae16976980e96e0e2902) (reverted from commit 498e0c7b3ad8a2f0cf23ef5b93df63ef5d720431)

--- BiznisSloj/BankarskiTecaj/TecajHNBa.cs
+++ BiznisSloj/BankarskiTecaj/TecajHNBa.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Windows;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace BiznisSloj.BankarskiTecaj
