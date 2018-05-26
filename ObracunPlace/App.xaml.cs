a27c94c63e41eb47b5e26de15e79bfc0bc67f2f1
NJUFRA-PC\Darko <darko.eler@petrokemija.hr>
2018-05-26 20:35:08 +0200

    ZavršnoOdKuće26.05

--- ObracunPlace/App.xaml.cs
+++ ObracunPlace/App.xaml.cs
@@ -1,9 +1,19 @@
-﻿namespace ObracunPlace
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace ObracunPlace
 {
     /// <summary>
     ///   Interaction logic for App.xaml
     /// </summary>
-    public partial class App
+    public partial class App 
     {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var friendlyMsg = $"Pa nešto je pošlo po zlu! Greška je u: [{e.Exception.Message}]";
+            var caption = "Pogreška!";
+            MessageBox.Show(friendlyMsg, caption, MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
